1633e7a8704b548306e26c6ae442555a007f7787
Nicolas Roy B <phatshambler@gmail.com>
2023-08-16 16:30:19 -0400

    Fixes

--- editors/article/src/Settings.jsx
+++ editors/article/src/Settings.jsx
@@ -1,7 +1,9 @@
-import { useFieldComponent } from '@niche-js/core/contexts';
 import classNames from 'classnames';
+import isArray from 'lodash/isArray';
 import PropTypes from 'prop-types';
 import React from 'react';
+
+import { useFieldComponent } from '@niche-js/core/contexts';
 
 import styles from './styles.module.css';
 
@@ -23,10 +25,18 @@ const defaultProps = {
 
 function Settings({ value, fields, onChange, className }) {
     const Fields = useFieldComponent('fields');
+    const { type = null } = value || {};
     return (
         <div className={classNames(['mw-100 p-2', { [className]: className !== null }])}>
-            <p className="text-capitalize mx-2">{value?.type}</p>
-            <Fields className={styles.fields} fields={fields} value={value} onChange={onChange} />
+            {type !== null ? <p className="text-capitalize mx-2">{type}</p> : null}
+            {Fields !== null && fields !== null && isArray(fields) ? (
+                <Fields
+                    className={styles.fields}
+                    fields={fields}
+                    value={value}
+                    onChange={onChange}
+                />
+            ) : null}
         </div>
     );
 }
