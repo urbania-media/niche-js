dbc9707af34d2c7735a2eb557df38214ff431883
Nicolas Roy B <phatshambler@gmail.com>
2023-08-16 16:12:54 -0400

    Fix

--- packages/ckeditor/src/useNicheEditor.js
+++ packages/ckeditor/src/useNicheEditor.js
@@ -1,4 +1,4 @@
-// import { ClickObserver } from '@ckeditor/ckeditor5-engine';
+import { ClickObserver } from '@ckeditor/ckeditor5-engine';
 import CKEditorInspector from '@ckeditor/ckeditor5-inspector';
 import { useEffect, useRef, useState } from 'react';
 
@@ -37,9 +37,10 @@ function useNicheEditor({ body, onChange = null, onClick = null, onFocus = null,
         }
         NicheEditor.create(currentContainer)
             .then((editor) => {
-                //  console.log('Editor was initialized', editor, body);
-                // const { view } = editor.editing;
-                // view.addObserver(ClickObserver);
+                console.log('Editor was initialized', editor, body);
+
+                const { view } = editor.editing;
+                view.addObserver(ClickObserver);
 
                 editor.setData(body);
 
