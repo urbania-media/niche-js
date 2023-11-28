9774a6be51fd875296803d4618e7c6a9744b1cf1
David Mongeau-Petitpas <dmongeau.petitpas@gmail.com>
2023-11-28 15:05:03 -0500

    Fix

---
+++ packages/intl/locale/en.cjs.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var manager = require('@niche-js/intl');
+
+var messages = {
+  "0aV7v8": [{
+    type: 0,
+    value: "Image label"
+  }],
+  "OPxJx/": [{
+    type: 0,
+    value: "Layout"
+  }]
+};
+
+/* eslint-disable */
+manager.addLocale('en', messages);
